953fa49845e0d63ba0b973f2c3e43c94aa720452
Michael Kutsenko <m.kutsenko.work@gmail.com>
2016-12-11 02:15:48 +0200

    created test directory

---
+++ src/main/java/ua/kiev/kvartirant/Test/Test.java
@@ -0,0 +1,17 @@
+package ua.kiev.kvartirant.Test;
+
+public class Test {
+    private long id;
+
+    public Test(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
